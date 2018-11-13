44d6aee997a840f912681c34d6e5a8028658f9c7
Wenyi Zheng <hilarywenyi@gmail.com>
2018-11-13 17:31:56 -0500

    added map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('running!');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaGlsYXJ5d2VueWlkZWFsIiwiYSI6ImNqb2dhc2M3aTAxYjgza3F0c3J0Zjk3MHMifQ.ndA30U5fdxtHEQuV-ZQdDQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
